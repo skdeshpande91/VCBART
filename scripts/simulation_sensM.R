a628968b9a120661175427d74280ada5b0af25e6
skdeshpande91 <sameerd@alum.mit.edu>
2021-05-10 15:08:23 -0500

    Update sensM simulation script again

--- scripts/simulation_sensM.R
+++ scripts/simulation_sensM.R
@@ -1,8 +1,8 @@
-# draw data from original p5R20
-# vary M from 1, 10, 50, 100
-# track beta_mse_test, ystar_mse_test, time
+# Simulation study to assess sensitivity to number of trees
+
 load("data/p5R20_data.RData")
 source("scripts/vcbart_wrapper.R")
+source("scripts/assess_support_recovery.R")
 
 args <- commandArgs(TRUE)
 M <- as.numeric(args[1])
@@ -14,6 +14,11 @@ fit <- vcbart_wrapper(Y_train, X_train, Z_train, n_train,
                         X_test, Z_test, n_test, cutpoints, M = M, error_structure = "ind",
                         split_probs_type = "adaptive", burn = 500, nd = 1000,
                         verbose = TRUE, print_every = 250)
+
+
+est_support <- fit$beta_support[["support"]][[1]]
+confusion_mat <- get_confusion_matrix(true_support, est_support, R)
+
 
 beta_mse_train <- colMeans( (beta_train - fit[["train"]][["beta"]][,"MEAN",])^2 )
 beta_mse_test <- colMeans( (beta_test - fit[["test"]][["beta"]][,"MEAN",])^2 )
@@ -31,5 +36,5 @@ vcbart_time <- fit$vcbart_time
 
 save_list <- c("beta_mse_train", "beta_mse_test", "beta_cov_train", "beta_cov_test", 
                "ystar_mse_train", "ystar_mse_test", "ystar_cov_train", "ystar_cov_test",
-               "time", "vcbart_time")
+               "time", "vcbart_time", "confusion_mat")
 save(list = save_list, file = paste0("results/sensM/results_sensM", M, "_sim", sim_number, ".RData"))
