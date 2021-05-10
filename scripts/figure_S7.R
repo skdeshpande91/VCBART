4292b69e70bd98e08628fc9b018ff3dfbba8e24c
skdeshpande91 <sameerd@alum.mit.edu>
2021-05-10 15:07:33 -0500

    Re-run sensM simulation to assess modifier selection

--- scripts/figure_S7.R
+++ scripts/figure_S7.R
@@ -17,7 +17,14 @@ time <- data.frame("M1" = M1_time,
                    "M50" = M50_time,
                    "M100" = M100_time)
 
+f1 <- data.frame("M1" = M1_varsel[,"f1"],
+                 "M25" = M25_varsel[,"f1"],
+                 "M50" = M50_varsel[,"f1"],
+                 "M100" = M100_varsel[,"f1"])
+
 #png("~/Documents/Research/vc_bart/figures/sensM_boxplot.png", width = 8, height = 8/3, units = "in", res = 400)
+#png("figures/sensM_boxplot.png", width = 8, height = 8/3, units = "in", res = 400)
+#par(mar = c(4.2,1,2,1), mgp  = c(1.8, 0.5, 0), mfrow = c(1,3), cex.main = 1, cex.axis = 1, cex.lab = 1)
 png("figures/sensM_boxplot.png", width = 8, height = 8/3, units = "in", res = 400)
 par(mar = c(4.2,1,2,1), mgp  = c(1.8, 0.5, 0), mfrow = c(1,3), cex.main = 1, cex.axis = 1, cex.lab = 1)
 boxplot(beta_mse, horizontal = TRUE, main = expression("Covariate effect recovery"), 
@@ -33,68 +40,17 @@ boxplot(ystar_rmse, horizontal = TRUE, main = expression("Predictive performance
 text(x = 2, y = 1, labels = "M = 1")
 text(x = 3.5, y = 2, labels = "M = 25")
 text(x = 3.5, y = 3, labels = "M = 50")
-text(x = 3.5, y = 4, labels = "M = 100")
+text(x = 4, y = 4, labels = "M = 100")
 mtext(text = "RMSE\n(b)", side = 1, line = 3, cex = 1 * par('cex'))
 
-boxplot(time/60, horizontal = TRUE, main = expression("Run time"), 
-        yaxt = "n", xlab = "", at = 1:4, medlwd = 0.5, pch = 16, cex = 0.8)
-text(x = 15, y = 1, labels = "M = 1")
-text(x = 20, y = 2, labels = "M = 25")
-text(x = 45, y = 3, labels = "M = 50")
-text(x = 50, y = 4, labels = "M = 100")
-mtext(text = "Minutes\n(c)", side = 1, line = 3, cex = 1 * par('cex'))
-dev.off()
-
-##############
-beta_mse_mean <- rep(NA, times = length(M_list))
-ystar_mse_mean <- rep(NA, times = length(M_list))
-time_mean <- rep(NA, times = length(M_list))
-
-for(i in 1:length(M_list)){
-  M <- M_list[i]
-  
-  tmp_beta <- get(paste0("M", M, "_beta_mse"))
-  tmp_beta_mean <- rowMeans(tmp_beta)
-  
-  beta_mse_mean[i] <- mean(tmp_beta_mean)
-  
-  ystar_mse_mean[i] <- mean(get(paste0("M", M, "_ystar_mse")))
-  
-  time_mean[i] <- mean(get(paste0("M", M, "_time")))
-}
-
-boxplot(beta_mse_mean[lin_method_names,], use.cols = FALSE, horizontal = TRUE,
-        main = expression("Covariate effect recovery"), xlab = "", yaxt = "n",
-        pch = 16, cex = 0.5, medlwd = 0.5, at = 1:5)
-log_M_list <- log(M_list)
-
-png("~/Documents/Research/vc_bart/figures/sensM_performance.png", width = 9, height = 3, units = "in", res = 600)
-par(mar = c(3,3,2,1), mgp = c(1.8, 0.5, 0), mfrow = c(1,3), cex.main = 1.1, cex.lab = 1.1, cex.axis = 1.1)
-plot(1, type = "n", xlim = range(log_M_list), ylim = c(0, max(beta_mse_mean)),
-     main = "Covariate effect recovery", ylab = "MSE", xlab = "M", xaxt = "n")
-axis(side = 1, at = log_M_list, labels = M_list)
-lines(log_M_list, beta_mse_mean)
-points(log_M_list, beta_mse_mean, pch = 16)
-
-plot(1, type = "n", xlim = range(log_M_list), ylim = c(0, max(ystar_mse_mean)),
-     main = "Predictive performance", ylab = "MSE", xlab = "M", xaxt = "n")
-axis(side = 1, at = log_M_list, labels = M_list)
-lines(log_M_list, ystar_mse_mean)
-points(log_M_list, ystar_mse_mean, pch = 16)
-
-log_time_list <- log(c(1, 5, 15, 30, 60, 90)) # time in minutes
-
-plot(1, type = "n", xlim = range(log_M_list), ylim = range(log_time_list),
-     main = "Run time (minutes)", ylab = "Time", xlab = "N", xaxt = "n", yaxt = "n")
-
-points(log_M_list, log(time_mean/60), pch = 16)
-
-#tmp_fit <- lm(log(time_mean/60) ~ log_M_list)
-#abline(a = tmp_fit$coefficients[1], b = tmp_fit$coefficients[2], lty = 2)
-
-axis(side = 1, at = log_M_list, labels = M_list)
-axis(side = 2, at = log_time_list, labels = c(1, 5, 15, 30, 60, 90))
-#legend("topleft", legend = paste0("slope = ", round(tmp_fit$coefficients[2], digits = 2)), lty = 2, cex = 1.1)
+boxplot(f1, horizontal = TRUE, main = expression("F1 score"),
+        yaxt = "n", xlab = "", at = 1:4, medlwd = 0.5, pch = 16, cex = 0.8, ylim = c(0,1))
+text(x = 0.3, y = 1, labels = "M = 1")
+text(x = 0.5, y = 2, labels = "M = 25")
+text(x = 0.5, y = 3, labels = "M = 50")
+text(x = 0.45, y = 4, labels = "M = 100")
+mtext(text = "F1\n(c)", side = 1, line = 3, cex = 1 * par('cex'))
 
 dev.off()
+
 
